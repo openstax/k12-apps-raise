59ae8584e0af9e6d1bdf8c24a914aad3b052b343
Ripal Nathuji <rnathuji@neoteklabs.com>
2022-04-08 19:13:49 -0500

    Update vite configuration with dev build options

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,23 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()]
+export default defineConfig(({ mode }) => {
+  const config = {
+    plugins: [react()],
+    build: {
+      sourcemap: true
+    }
+  }
+
+  // In development mode don't include a hash in the filename
+  if (mode === 'development') {
+    config.build['rollupOptions'] = {
+      output: {
+        assetFileNames: 'assets/[name][extname]',
+        entryFileNames: 'assets/[name].js'
+      }
+    }
+  }
+
+  return config
 })
