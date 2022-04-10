2a6a1f3d6d3a543f6ca9f7fded39c9dc51de378b
Ripal Nathuji <rnathuji@neoteklabs.com>
2022-04-10 12:57:37 -0500

    Setup namespaced bootstrap for styling

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,5 @@
 import { renderContentElements } from './lib/content'
+import './styles/main.scss'
 
 const processPage = async (): Promise<void> => {
   // Load any content that needs to be fetched and inserted into the page
