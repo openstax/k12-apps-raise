090f86eaaddebeddf751d28cab9bc19f3bb663fb
Prabhdip Gill <gillprabhdip@gmail.com>
2022-04-19 12:56:16 -0700

    changed test to test mouseover event and tooltip html

--- src/tests/TooltipBlock.test.tsx
+++ src/tests/TooltipBlock.test.tsx
@@ -1,13 +1,16 @@
 import '@testing-library/jest-dom'
-import { render, screen } from '@testing-library/react'
+import { fireEvent, render, screen } from '@testing-library/react'
 import { TooltipBlock } from '../components/TooltipBlock'
 
 test('Tooltip Block unit test', async () => {
   render(
     <div data-testid="tooltip-content">
-      <TooltipBlock text="my-text" tip="my-tip"/>
+      <TooltipBlock text="my-text" tip="<p>my-tip</p>"/>
     </div>
   )
 
   expect(screen.getByTestId('tooltip-content')).toHaveTextContent('my-text')
+  fireEvent.mouseOver(screen.getByText('my-text'))
+  expect(await screen.findByText('my-tip')).toBeVisible()
+  expect(document.querySelector('.os-raise-bootstrap')).toHaveTextContent('my-tip')
 })
