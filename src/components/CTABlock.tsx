bdbea647bacb85f865bb1b02984d735ace76f842
Prabhdip Gill <gillprabhdip@gmail.com>
2022-04-20 12:33:44 -0700

    added onclick event and conditional rendering.

--- src/components/CTABlock.tsx
+++ src/components/CTABlock.tsx
@@ -1,9 +1,38 @@
+import { useState } from 'react'
+
 interface CTABlockProps {
+  contentString: string
+  contentPrompt: string
   buttonText: string
+  firesEvent?: string
+
 }
 
-export const CTABlock = ({ buttonText }: CTABlockProps): JSX.Element => {
+export const CTABlock = ({ contentString, contentPrompt, buttonText, firesEvent }: CTABlockProps): JSX.Element => {
+  const [clicked, setclicked] = useState<boolean>(false)
+
+  const clickHandler = (): void => {
+    if (firesEvent !== undefined) {
+      const clickEvent = new CustomEvent(firesEvent)
+      document.dispatchEvent(clickEvent)
+    }
+    setclicked(true)
+  }
+
+  const promptButton = (): JSX.Element => {
+    if (!clicked) {
+      return (<>
+      <div dangerouslySetInnerHTML={{ __html: contentPrompt }} />
+      <button onClick={clickHandler} type="button">{buttonText}</button></>)
+    } else {
+      return <></>
+    }
+  }
+
   return (
-    <button type="button">{buttonText}</button>
+    <>
+      <div dangerouslySetInnerHTML={{ __html: contentString }} />
+    {promptButton()}
+    </>
   )
 }
--- src/tests/CTABlock.test.tsx
+++ src/tests/CTABlock.test.tsx
@@ -1,14 +1,45 @@
-import { render, screen } from '@testing-library/react'
+import { fireEvent, render, screen } from '@testing-library/react'
 import { CTABlock } from '../components/CTABlock'
 import '@testing-library/jest-dom'
 
 test('CTABlock renders', async () => {
   render(
     <div data-testid="cta-block">
-      <CTABlock buttonText="Click me!"/>
+      <CTABlock buttonText="Click me!" contentString={'<p>String</p>'} contentPrompt={'<p>Prompt</p>'}/>
     </div>
   )
+  const queryList = Array.from(screen.getByTestId('cta-block').querySelectorAll('p'))
 
   expect(screen.getByTestId('cta-block').querySelector('button')).not.toBeNull()
   expect(screen.getByTestId('cta-block')).toHaveTextContent('Click me!')
+  expect(queryList[0]).toHaveTextContent('String')
+  expect(queryList[1]).toHaveTextContent('Prompt')
+})
+
+test('CTABlock fires event', async () => {
+  render(
+    <div data-testid="cta-block">
+      <CTABlock buttonText="Click me!" contentString={'<p>String</p>'} contentPrompt={'<p>Prompt</p>'} firesEvent={'Event'}/>
+    </div>
+  )
+  const eventHandler = jest.fn()
+
+  document.addEventListener('Event', eventHandler)
+  fireEvent.click(screen.getByText('Click me!'))
+  expect(eventHandler).toHaveBeenCalled()
+})
+
+test('CTABlock button prompt disappear', async () => {
+  render(
+    <div data-testid="cta-block">
+      <CTABlock buttonText="Click me!" contentString={'<p>String</p>'} contentPrompt={'<p>Prompt</p>'} firesEvent={'Event'}/>
+    </div>
+  )
+
+  fireEvent.click(screen.getByText('Click me!'))
+  const queryList = Array.from(screen.getByTestId('cta-block').querySelectorAll('p'))
+
+  expect(screen.getByTestId('cta-block')).not.toHaveTextContent('Click me!')
+  expect(queryList.length === 1)
+  expect(queryList[0]).toHaveTextContent('String')
 })
