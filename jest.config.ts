55a265dcdb1a16abb2d7fcf94753317a0cf74618
Ripal Nathuji <rnathuji@neoteklabs.com>
2022-04-29 08:59:55 -0500

    Measure Jest coverage on all source files

--- jest.config.ts
+++ jest.config.ts
@@ -3,5 +3,10 @@ export default {
   preset: 'ts-jest',
   testMatch: ['**/src/tests/*.ts?(x)'],
   collectCoverage: true,
+  collectCoverageFrom: [
+    'src/**/*.ts?(x)',
+    '!src/*.d.ts',
+    '!src/lib/env.ts'
+  ],
   verbose: true
 }
