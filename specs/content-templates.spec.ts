621cf8711163e145d66e4442ebd8390110948f3f
Prabhdip Gill <gillprabhdip@gmail.com>
2022-05-03 13:43:36 -0700

    Add tooltip test to user input block test.

--- specs/content-templates.spec.ts
+++ specs/content-templates.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test'
 import { mockPageContentRequest } from './utils'
 
-test('user input block can trigger content-only block', async ({ page }) => {
+test('user input block can trigger content-only block and tooltip', async ({ page }) => {
   const htmlContent = `
   <div class="os-raise-ib-input" data-fire-event="eventnameX" data-schema-version="1.0">
     <div class="os-raise-ib-input-content"></div>
@@ -10,6 +10,7 @@ test('user input block can trigger content-only block', async ({ page }) => {
   </div>
   <div class="os-raise-ib-content" data-wait-for-event="eventnameX" data-schema-version="1.0">
     <p>Conditional content</p>
+    <p>A sentence with the word <span class="os-raise-ib-tooltip" data-store="glossary-tooltip">absolute value</span> with its definition as a tooltip</p>
   </div>
   `
 
@@ -19,4 +20,7 @@ test('user input block can trigger content-only block', async ({ page }) => {
   await page.fill('textarea', 'Response text')
   await page.locator('text=Submit').click()
   await page.waitForSelector('text=Conditional content')
+  await page.locator('text=absolute value').hover()
+  await page.waitForSelector('text=Coming soon!Math: \\( E=mc^2 \\)')
+  await page.waitForSelector('.MathJax')
 })
