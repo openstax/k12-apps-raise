4b02915764391b20fc0aa7b9991b505074eefac8
Ripal Nathuji <rnathuji@neoteklabs.com>
2022-05-06 08:46:09 -0500

    Remove unused import

--- src/lib/blocks.tsx
+++ src/lib/blocks.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import { createRoot } from 'react-dom/client'
 import { ContentBlock } from '../components/ContentBlock'
 import { CTABlock } from '../components/CTABlock'
