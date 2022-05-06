88bf86247c13b8cf0d86d181951772decaa185a8
Ripal Nathuji <rnathuji@neoteklabs.com>
2022-05-06 10:25:50 -0500

    Render problem set blocks via loader and moodle

--- src/lib/blocks.tsx
+++ src/lib/blocks.tsx
@@ -22,7 +22,8 @@ export const isInteractiveBlock = (element: HTMLElement): boolean => {
   return [
     OS_RAISE_IB_CONTENT_CLASS,
     OS_RAISE_IB_CTA_CLASS,
-    OS_RAISE_IB_INPUT_CLASS
+    OS_RAISE_IB_INPUT_CLASS,
+    OS_RAISE_IB_PSET_CLASS
   ].some(blockClass => element.classList.contains(blockClass))
 }
 
@@ -85,6 +86,13 @@ const blockifyElement = (element: HTMLElement): JSX.Element => {
     const maybeUserInputBlock = parseUserInputBlock(element)
     if (maybeUserInputBlock !== null) {
       return maybeUserInputBlock
+    }
+  }
+
+  if (element.classList.contains(OS_RAISE_IB_PSET_CLASS)) {
+    const maybeProblemSetBlock = parseProblemSetBlock(element)
+    if (maybeProblemSetBlock !== null) {
+      return maybeProblemSetBlock
     }
   }
 
@@ -263,4 +271,8 @@ export const renderContentOnlyBlocks = (element: HTMLElement): void => {
 
 export const renderUserInputBlocks = (element: HTMLElement): void => {
   renderContentBlocksByClass(element, OS_RAISE_IB_INPUT_CLASS, parseUserInputBlock)
+}
+
+export const renderProblemSetBlocks = (element: HTMLElement): void => {
+  renderContentBlocksByClass(element, OS_RAISE_IB_PSET_CLASS, parseProblemSetBlock)
 }
--- src/tests/blocks.test.ts
+++ src/tests/blocks.test.ts
@@ -8,9 +8,12 @@ import {
   renderUserInputBlocks,
   OS_RAISE_IB_CTA_CLASS,
   OS_RAISE_IB_CONTENT_CLASS,
-  OS_RAISE_IB_INPUT_CLASS
+  OS_RAISE_IB_INPUT_CLASS,
+  OS_RAISE_IB_PSET_CLASS,
+  renderProblemSetBlocks
 } from '../lib/blocks'
 import { UserInputBlock } from '../components/UserInputBlock'
+import { ProblemSetBlock } from '../components/ProblemSetBlock'
 
 const mockRender = jest.fn()
 
@@ -57,6 +60,17 @@ test('isInteractiveBlock recognizes user input block', async () => {
   expect(isInteractiveBlock(tmpDiv)).toBe(true)
 })
 
+test('isInteractiveBlock recognizes problem set block', async () => {
+  const tmpDiv = document.createElement('div')
+  tmpDiv.className = OS_RAISE_IB_PSET_CLASS
+
+  expect(isInteractiveBlock(tmpDiv)).toBe(true)
+
+  tmpDiv.className = `${OS_RAISE_IB_PSET_CLASS} otherclass`
+
+  expect(isInteractiveBlock(tmpDiv)).toBe(true)
+})
+
 test('blockifyHTML consolidates non-block peer elements into a single content block component', async () => {
   const htmlContent = '<p>P1</p><p>P2</p><p>P3</p>'
   const components = blockifyHTML(htmlContent)
@@ -75,7 +89,7 @@ test('blockifyHTML handles non-block content with interactive blocks', async () 
     </div>
     <div class="os-raise-ib-cta-prompt">
       <p>Want to see more?</p>
-    </div>  
+    </div>
   </div>
   <div class="${OS_RAISE_IB_CONTENT_CLASS}" data-wait-for-event="event">Test waiting</div>
   <div class="${OS_RAISE_IB_CONTENT_CLASS}">Test no wait</div>
@@ -94,6 +108,27 @@ test('blockifyHTML handles non-block content with interactive blocks', async () 
   expect(components[3].props.children.type).toBe(ContentBlock)
   expect(components[4].props.children.type).toBe(UserInputBlock)
   expect(components[5].props.children.type).toBe(ContentBlock)
+})
+
+test('blockifyHTML handles problem set blocks', async () => {
+  const htmlContent = `
+  <div class="${OS_RAISE_IB_PSET_CLASS}" data-schema-version="1.0">
+    <div class="os-raise-ib-pset-problem" data-problem-type="input" data-solution="42" data-problem-comparator="integer">
+      <div class="os-raise-ib-pset-problem-content"></div>
+    </div>
+    <div class="os-raise-ib-pset-problem" data-problem-type="dropdown" data-solution="red" data-solution-options='["red", "blue", "green"]'>
+      <div class="os-raise-ib-pset-problem-content"></div>
+    </div>
+    <div class="os-raise-ib-pset-problem" data-problem-type="multiselect" data-solution='["red", "blue"]' data-solution-options='["red", "blue", "green"]'>
+      <div class="os-raise-ib-pset-problem-content"></div>
+    </div>
+    <div class="os-raise-ib-pset-correct-response"></div>
+    <div class="os-raise-ib-pset-encourage-response"></div>
+  </div>
+  `
+  const components = blockifyHTML(htmlContent)
+  expect(components.length).toBe(1)
+  expect(components[0].props.children.type).toBe(ProblemSetBlock)
 })
 
 test('renderContentOnlyBlocks parses and creates expected block', async () => {
@@ -127,5 +162,29 @@ test('renderUserInputBlocks parses and creates expected block', async () => {
   document.body.appendChild(divElem)
 
   renderUserInputBlocks(document.body)
+  expect(createRoot).toBeCalledWith(divElem)
+})
+
+test('renderProblemSetBlocks parses and creates expected block', async () => {
+  const divElem = document.createElement('div')
+  divElem.className = OS_RAISE_IB_PSET_CLASS
+  divElem.innerHTML = `
+  <div class="${OS_RAISE_IB_PSET_CLASS}" data-schema-version="1.0">
+    <div class="os-raise-ib-pset-problem" data-problem-type="input" data-solution="42" data-problem-comparator="integer">
+      <div class="os-raise-ib-pset-problem-content"></div>
+    </div>
+    <div class="os-raise-ib-pset-problem" data-problem-type="dropdown" data-solution="red" data-solution-options='["red", "blue", "green"]'>
+      <div class="os-raise-ib-pset-problem-content"></div>
+    </div>
+    <div class="os-raise-ib-pset-problem" data-problem-type="multiselect" data-solution='["red", "blue"]' data-solution-options='["red", "blue", "green"]'>
+      <div class="os-raise-ib-pset-problem-content"></div>
+    </div>
+    <div class="os-raise-ib-pset-correct-response"></div>
+    <div class="os-raise-ib-pset-encourage-response"></div>
+  </div>
+  `
+  document.body.appendChild(divElem)
+
+  renderProblemSetBlocks(document.body)
   expect(createRoot).toBeCalledWith(divElem)
 })
