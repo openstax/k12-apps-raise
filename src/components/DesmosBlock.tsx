602beb56ce1fe0b9cfd5a2e4d778089426806b55
Prabhdip Gill <gillprabhdip@gmail.com>
2022-05-23 13:04:32 -0700

    Create DesmosBlock.tsx file

---
+++ src/components/DesmosBlock.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+interface DesmosBlockProps{
+  width: number
+  height: number
+}
+export default function DesmosBlock({ width, height }: DesmosBlockProps): JSX.Element {
+  return (
+    <div>DesmosBlock</div>
+  )
+}
