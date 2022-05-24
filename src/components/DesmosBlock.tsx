a5ab232d6e89ad21fd61ebc9d6cec48a8ca129bf
Prabhdip Gill <gillprabhdip@gmail.com>
2022-05-24 14:07:29 -0700

    Add desmos code to DesmosBlock tsx

--- src/components/DesmosBlock.tsx
+++ src/components/DesmosBlock.tsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
+import Desmos from 'desmos'
 
-interface DesmosBlockProps{
-  width: number
-  height: number
+interface DesmosBlockProps {
+  width: string
+  height: string
 }
-export default function DesmosBlock({ width, height }: DesmosBlockProps): JSX.Element {
+export function DesmosBlock({ width, height }: DesmosBlockProps): JSX.Element {
+  const wrapperEl = useRef<HTMLDivElement | null>(null)
+
+  useEffect(() => {
+    const maybeWrapper = wrapperEl.current
+    if (maybeWrapper === null) {
+      return
+    }
+    maybeWrapper.style.width = '600px'
+    maybeWrapper.style.height = '400px'
+    const calculator = Desmos.GraphingCalculator(wrapperEl)
+    calculator.setExpression({ id: 'graph1', latex: 'y=x^2' })
+  }, [])
+
   return (
-    <div>DesmosBlock</div>
+    <div className="os-raise-bootstrap">
+      <div ref={wrapperEl}/>
+
+    </div>
   )
 }
