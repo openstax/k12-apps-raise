51e52798d803d27adee66c30c85f8c7fa3b6862d
Prabhdip Gill <gillprabhdip@gmail.com>
2022-06-14 08:38:34 -0700

    Remove unused desmos npm library

--- src/tests/DesmosBlock.test.tsx
+++ src/tests/DesmosBlock.test.tsx
@@ -6,13 +6,11 @@ import { DesmosBlock } from '../components/DesmosBlock'
 jest.mock('../lib/desmos.ts', () => ({
   loadDesmos: jest.fn()
 }))
-jest.mock('Desmos', () => ({
-  GraphingCalculator: jest.fn()
-}))
 
 test('Desmos block calls script', async () => {
   render(
       <DesmosBlock width={'500'} height={'500'} equations={'["x=5", "(1,2)"]'} expressions={'true'} scaleTop={'10'} scaleBottom={'-10'} scaleLeft={'-10'} scaleRight={'10'} />
   )
+
   expect(loadDesmos).toBeCalled()
 })
