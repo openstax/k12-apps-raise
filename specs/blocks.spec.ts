b8eb05d11dcaf9122330434aa541b9609503474b
Prabhdip Gill <gillprabhdip@gmail.com>
2022-06-15 11:41:36 -0700

    Remove duplicate test

--- specs/blocks.spec.ts
+++ specs/blocks.spec.ts
@@ -323,15 +323,6 @@ test('Desmos has equations and shows all in expressions panel.', async ({ page }
   await page.waitForSelector('text=(1,2)')
 })
 
-test('Desmos has equations and shows all in expressions panel.', async ({ page }) => {
-  const htmlDesmosContent = '<div class="os-raise-ib-desmos" data-expressions="true" data-width="600" data-top="50" data-bottom="-50" data-left="-50" data-right="50" data-height="500" data-equations=\'["(1,2)", "x=5"]\'></div>'
-  await mockPageContentRequest(page, htmlDesmosContent)
-  await page.goto('/')
-  await page.waitForSelector('.dcg-grapher')
-  await page.waitForSelector('text=x=5')
-  await page.waitForSelector('text=(1,2)')
-})
-
 test('Desmos expressions panel is not visible.', async ({ page }) => {
   const htmlDesmosContent = '<div class="os-raise-ib-desmos" data-expressions="false" data-width="600" data-top="50" data-bottom="-50" data-left="-50" data-right="50" data-height="500" data-equations=\'["(1,2)", "x=5"]\'></div>'
   await mockPageContentRequest(page, htmlDesmosContent)
