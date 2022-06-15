0b1c97a2fd6fda96b71a8fe94e838f61c3d4e8b3
Prabhdip Gill <gillprabhdip@gmail.com>
2022-06-15 11:52:18 -0700

    Fix plawright test race condition

--- specs/blocks.spec.ts
+++ specs/blocks.spec.ts
@@ -304,14 +304,16 @@ test('Desmos script loads', async ({ page }) => {
 
   await mockPageContentRequest(page, htmlDesmosContent)
   await page.goto('/')
+  await page.waitForSelector('.dcg-grapher')
   expect(await page.locator(`script[src="${DESMOS_URL}"]`).count()).toBe(1)
 })
-test('', async ({ page }) => {
-  const htmlDesmosContent = '<div class="os-raise-ib-desmos" data-expressions="false" data-width="600" data-top="50" data-bottom="-50" data-left="-50" data-right="50" data-height="500" data-equations=\'["(1,2)", "x=5"]\'></div>' +
-  '<div class="os-raise-ib-desmos" data-expressions="false" data-width="600" data-top="50" data-bottom="-50" data-left="-50" data-right="50" data-height="500" data-equations=\'["(1,2)", "x=5"]\'></div>'
+test('There is only one Desmos script for multiple calculators', async ({ page }) => {
+  const htmlDesmosContent = `<div class="os-raise-ib-desmos" data-expressions="false" data-width="600" data-top="50" data-bottom="-50" data-left="-50" data-right="50" data-height="500" data-equations='["(1,2)", "x=5"]'></div>
+  <div class="os-raise-ib-desmos" data-expressions="false" data-width="600" data-top="50" data-bottom="-50" data-left="-50" data-right="50" data-height="500" data-equations='["(1,2)", "x=5"]'></div>`
 
   await mockPageContentRequest(page, htmlDesmosContent)
   await page.goto('/')
+  await page.waitForSelector('.dcg-grapher')
   expect(await page.locator(`script[src="${DESMOS_URL}"]`).count()).toBe(1)
 })
 test('Desmos has equations and shows all in expressions panel.', async ({ page }) => {
