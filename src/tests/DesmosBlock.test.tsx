9ec9621564d6847498583759e075e724435a07c8
Prabhdip Gill <gillprabhdip@gmail.com>
2022-06-22 12:39:57 -0700

    Add waitForEvent tests to DesmosBlock tests

--- src/tests/DesmosBlock.test.tsx
+++ src/tests/DesmosBlock.test.tsx
@@ -1,5 +1,5 @@
 import '@testing-library/jest-dom'
-import { render, waitFor } from '@testing-library/react'
+import { render, waitFor, fireEvent } from '@testing-library/react'
 import { loadDesmos } from '../lib/desmos'
 import { DesmosBlock } from '../components/DesmosBlock'
 
@@ -25,6 +25,56 @@ test('Desmos block calls script and grahing calcultor functions', async () => {
   )
 
   expect(loadDesmos).toBeCalled()
+
+  await waitFor(() => expect(graphingCalculator).toBeCalled())
+  expect(setExpression).toBeCalled()
+  expect(setMathBounds).toBeCalled()
+})
+
+test('DesmosBlock does not render if waitForEvent does not fire', async () => {
+  const setExpression = jest.fn()
+  const setMathBounds = jest.fn()
+  const graphingCalculator = jest.fn(() => {
+    return {
+      setExpression: setExpression,
+      setMathBounds: setMathBounds
+    }
+  })
+  window.Desmos = {
+    GraphingCalculator: graphingCalculator
+  } as any
+
+  render(
+      <DesmosBlock waitForEvent='event' width={'500'} height={'500'} equations={'["x=5", "(1,2)"]'} expressions={'true'} scaleTop={'10'} scaleBottom={'-10'} scaleLeft={'-10'} scaleRight={'10'} />
+  )
+
+  expect(loadDesmos).toBeCalled()
+
+  await waitFor(() => expect(graphingCalculator).not.toBeCalled())
+  expect(setExpression).not.toBeCalled()
+  expect(setMathBounds).not.toBeCalled()
+})
+
+test('DesmosBlock calculator does render if waitForEvent does not fire', async () => {
+  const setExpression = jest.fn()
+  const setMathBounds = jest.fn()
+  const graphingCalculator = jest.fn(() => {
+    return {
+      setExpression: setExpression,
+      setMathBounds: setMathBounds
+    }
+  })
+  window.Desmos = {
+    GraphingCalculator: graphingCalculator
+  } as any
+
+  render(
+      <DesmosBlock waitForEvent='event' width={'500'} height={'500'} equations={'["x=5", "(1,2)"]'} expressions={'true'} scaleTop={'10'} scaleBottom={'-10'} scaleLeft={'-10'} scaleRight={'10'} />
+  )
+
+  expect(loadDesmos).toBeCalled()
+
+  fireEvent(document, new CustomEvent('event'))
 
   await waitFor(() => expect(graphingCalculator).toBeCalled())
   expect(setExpression).toBeCalled()
