ae55c5e160c792d7aa14d9b0f707e7f7758f1aad
Ripal Nathuji <rnathuji@neoteklabs.com>
2022-06-30 08:17:27 -0500

    Add work around for buggy moodle mathjax filter behavior
    The Moodle mathjax filter parses all stored content as a string in
    the PHP backend and transforms any content it finds with math
    delimiters. Unfortunately this ends up impacting all content including
    HTML comments, attributes, etc., resulting content preview errors for
    content devs that write valid HTML for our interactive blocks. This
    change includes a runtime "patch" that basically tries to undo the
    damage that the Moodle filter plugin does on the backend.

--- src/lib/blocks.tsx
+++ src/lib/blocks.tsx
@@ -308,11 +308,60 @@ const replaceElementWithBlock = (element: HTMLElement, component: JSX.Element): 
   )
 }
 
+const patchMoodleMathJaxFilterBreakage = (element: HTMLElement): void => {
+  // k12-189: This incredibly horrific function is a hacked together work around
+  // for buggy behavior in Moodle's MathJax filter plugin implementation where
+  // it ends up breaking data attributes in pset problem definitions that may
+  // have MathJax math (e.g. multiplechoice and multiselect options). This
+  // patching is only exercised on code paths where content is stored in Moodle
+  // versus loaded via ContentLoader. It should be removed when we no longer
+  // require supporting content preview with Moodle storage.
+
+  const patchContent = (content: string): string => {
+    let result = content
+
+    result = result.replaceAll('<span class="nolink">', '')
+    result = result.replaceAll('<span class=" nolink"="">', '')
+    result = result.replaceAll('</span>', '')
+    result = result.replaceAll('&gt;', '>')
+    result = result.replaceAll('"\\\\(', '&quot;\\\\(')
+    result = result.replaceAll('\\\\)"', '\\\\)&quot;')
+
+    return result
+  }
+
+  // The way HTML gets broken by the filter transformation causes the content
+  // to not be properly parsed, so we have to resort to modifying things as a
+  // string
+  const originalContent = element.innerHTML
+  let patchedContent = ''
+
+  // There are potential corner cases where this may not work or may have
+  // unintended side effects, but to try and limit the scope of impact we
+  // iterate through each line of the string and only try to "patch" those that
+  // have data-solution (which includes data-solution-options)
+
+  originalContent.split('\n').forEach((contentLine) => {
+    if (contentLine.includes('data-solution')) {
+      patchedContent += patchContent(contentLine)
+    } else {
+      patchedContent += contentLine
+    }
+  })
+
+  element.innerHTML = patchedContent
+}
+
 const renderContentBlocksByClass = (element: HTMLElement, contentClass: string, parser: (element: HTMLElement) => JSX.Element | null): void => {
   const contentItems = element.querySelectorAll(`.${contentClass}`)
 
   contentItems.forEach(elem => {
     const htmlElem = elem as HTMLElement
+
+    // Special case moodle storage patching for PSET blocks only
+    if (contentClass === OS_RAISE_IB_PSET_CLASS) {
+      patchMoodleMathJaxFilterBreakage(htmlElem)
+    }
     const maybeContentOnlyBlock = parser(htmlElem)
 
     if (maybeContentOnlyBlock === null) {
