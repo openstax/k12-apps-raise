5acfe935469116cd729334d60e164c8dc4566371
mjoss17 <matthewjoss17@gmail.com>
2022-11-14 15:02:37 -0600

    Add EventsAPI Client code

---
+++ src/eventsapi/models/index.ts
@@ -0,0 +1,9 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './ContentLoadFailedV1';
+export * from './ContentLoadedV1';
+export * from './DetailMessage';
+export * from './EventsInner';
+export * from './HTTPValidationError';
+export * from './LocationInner';
+export * from './ValidationError';
