1bc708216b4e5f8b35345c1d48897c13ecc093f7
Prabhdip Gill <pg53@rice.edu>
2022-12-02 12:35:59 -0800

    Fix weird github repository ghost file

--- src/moodleapi/MoodleApi.ts
+++ src/moodleapi/MoodleApi.ts
@@ -1,46 +1,45 @@
 interface GetUserResponse {
-    uuid: string
-    jwt: string
+  uuid: string
+  jwt: string
+}
+
+export class MoodleApi {
+  private readonly wwwroot: string
+  private readonly sesskey: string
+
+  constructor(wwwroot: string, sesskey: string) {
+    this.wwwroot = wwwroot
+    this.sesskey = sesskey
   }
-  
-  export class MoodleApi {
-    private readonly wwwroot: string
-    private readonly sesskey: string
-  
-    constructor(wwwroot: string, sesskey: string) {
-      this.wwwroot = wwwroot
-      this.sesskey = sesskey
-    }
-  
-    async getUser(): Promise<GetUserResponse> {
-      const methodname = 'local_raise_get_user'
-      const userRequest = {
-        index: 0,
-        methodname: methodname,
-        args: {
-        }
-      }
-      const url = this.getMoodleAjaxUrl(methodname)
-      const response = await fetch(url, { method: 'POST', body: JSON.stringify([userRequest]) })
-      const responseJSON = await response.json()
-  
-      if (responseJSON.error !== undefined) {
-        throw new Error(responseJSON.error)
-      }
-  
-      return {
-        uuid: responseJSON[0].data.uuid,
-        jwt: responseJSON[0].data.jwt
+
+  async getUser(): Promise<GetUserResponse> {
+    const methodname = 'local_raise_get_user'
+    const userRequest = {
+      index: 0,
+      methodname: methodname,
+      args: {
       }
     }
-  
-    getMoodleAjaxUrl(endpointMethod: string): string {
-      let userUrl: string
-      userUrl = this.wwwroot
-      userUrl += '/lib/ajax/service.php?sesskey='
-      userUrl += this.sesskey
-      userUrl += '&info=' + endpointMethod
-      return userUrl
+    const url = this.getMoodleAjaxUrl(methodname)
+    const response = await fetch(url, { method: 'POST', body: JSON.stringify([userRequest]) })
+    const responseJSON = await response.json()
+
+    if (responseJSON.error !== undefined) {
+      throw new Error(responseJSON.error)
+    }
+
+    return {
+      uuid: responseJSON[0].data.uuid,
+      jwt: responseJSON[0].data.jwt
     }
   }
-  
+
+  getMoodleAjaxUrl(endpointMethod: string): string {
+    let userUrl: string
+    userUrl = this.wwwroot
+    userUrl += '/lib/ajax/service.php?sesskey='
+    userUrl += this.sesskey
+    userUrl += '&info=' + endpointMethod
+    return userUrl
+  }
+}
