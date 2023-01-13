3e2972275f26284083338e1c9e3b7a3c56f4d842
Ripal Nathuji <rnathuji@neoteklabs.com>
2023-01-13 12:13:55 -0600

    Update env usage for new conventions in Vite 4
    There were a couple of improvements in the updated version of Vite
    around decoupling build modes and NODE_ENV:
    
    * https://github.com/vitejs/vite/pull/10996
    * https://github.com/vitejs/vite/pull/11045
    
    Accordingly, this change:
    
    * Consistently uses import.meta.env.MODE to set build mode specific
      values vs relying on the (previous) behavior around how
      import.meta.env.PROD was set.
    * Adds a development environment file so NODE_ENV is set to development
      for this builds.

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -1,10 +1,10 @@
 export const ENV = {
-  OS_RAISE_CONTENT_URL_PREFIX: import.meta.env.PROD ? 'https://k12.openstax.org/contents/raise' : 'http://localhost:8800/contents',
+  OS_RAISE_CONTENT_URL_PREFIX: import.meta.env.MODE === 'production' ? 'https://k12.openstax.org/contents/raise' : 'http://localhost:8800/contents',
   OS_RAISE_EVENTSAPI_URL_MAP: {},
   EVENT_FLUSH_PERIOD: 60000
 }
 
-if (import.meta.env.PROD) {
+if (import.meta.env.MODE === 'production') {
   ENV.OS_RAISE_EVENTSAPI_URL_MAP = {
     'raiselearning.org': 'https://events.raiselearning.org',
     'staging.raiselearning.org': 'https://events.staging.raiselearning.org'
