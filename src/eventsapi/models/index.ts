64cfc2ac802723eeb5fdb4463b64caf6afa5a08e
Ripal Nathuji <rnathuji@neoteklabs.com>
2023-02-09 08:39:10 -0600

    Update autogenerated eventsapi client code

--- src/eventsapi/models/index.ts
+++ src/eventsapi/models/index.ts
@@ -5,5 +5,8 @@ export * from './ContentLoadedV1';
 export * from './DetailMessage';
 export * from './EventsInner';
 export * from './HTTPValidationError';
+export * from './IbInputSubmittedV1';
+export * from './IbPsetProblemAttemptedV1';
 export * from './LocationInner';
+export * from './Response';
 export * from './ValidationError';
