e5169b89951add388f19365a9dbce04ee33b831b
Michael <mreyna12@live.com>
2023-05-25 09:54:37 -0500

    Update styles and fix linter errors

--- src/components/CustomCheckbox.tsx
+++ src/components/CustomCheckbox.tsx
@@ -1,4 +1,5 @@
 import { Field } from 'formik'
+import { CorrectAnswerIcon, WrongAnswerIcon } from './Icons'
 
 interface CheckboxProps {
   label: string
@@ -25,47 +26,15 @@ export const Checkbox = ({
   const selectedWrongAnswer = selected && !correct && showAnswer
 
   return (
-    <div className="os-raise-flex os-raise-align-items-center">
+    <div className={`os-raise-flex os-raise-align-items-center ${showAnswer ? 'disabled' : ''}`}>
       {selectedCorrectAnswer
-        ? (
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          className="os-raise-ml-1"
-        >
-          <path
-            fill="#019920"
-            d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10s10-4.48 10-10S17.52 2 12 2zm-2 15l-5-5l1.41-1.41L10 14.17l7.59-7.59L19 8l-9 9z"
-          />
-        </svg>
-          )
-        : (
-        <></>
-          )}
+        ? <CorrectAnswerIcon />
+        : <></>}
       {selectedWrongAnswer
-        ? (
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          className="os-raise-ml-1"
-        >
-          <path
-            fill="#fc0516"
-            d="M12 2c5.53 0 10 4.47 10 10s-4.47 10-10 10S2 17.53 2 12S6.47 2 12 2m3.59 5L12 10.59L8.41 7L7 8.41L10.59 12L7 15.59L8.41 17L12 13.41L15.59 17L17 15.59L13.41 12L17 8.41L15.59 7Z"
-          />
-        </svg>
-          )
-        : (
-        <></>
-          )}
+        ? <WrongAnswerIcon />
+        : <></>}
       <label
-        className={`form-check-label os-raise-fill-label-container ${
-          showAnswer ? 'os-raise-no-cursor-pointer' : ''
-        }`}
+        className="form-check-label os-raise-fill-label-container"
       >
         <Field
           className={`os-form-check-input ${
