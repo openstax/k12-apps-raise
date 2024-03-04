a980edc1d861cf4b1ac3b94beb1f77d41a4ba368
Michael Reyna <MReyna12@live.com>
2024-03-04 13:05:03 -0600

    Remove console logs

--- src/components/AttemptsCounter.tsx
+++ src/components/AttemptsCounter.tsx
@@ -4,8 +4,6 @@ interface Props {
 }
 
 export const AttemptsCounter = ({ retryLimit, retriesAllowed }: Props): JSX.Element => {
-  console.log('retryLimit:', retryLimit)
-  console.log('retriesAllowed:', retriesAllowed)
   return (
     <div className="os-flex os-justify-content-end">
       <p className="os-attempts-text">
