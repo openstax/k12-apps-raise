f00ab01ad6fcbf92cac690413bbb1a3f785ece1b
Michael Reyna <MReyna12@live.com>
2024-04-12 16:16:12 -0500

    Create initial test for fetch and display of results

---
+++ src/tests/SearchBlock.test.tsx
@@ -0,0 +1,103 @@
+import { act, fireEvent, render, screen } from '@testing-library/react'
+import { SearchBlock } from '../components/SearchBlock'
+
+const mockQueryResults = {
+  took: 55,
+  timed_out: false,
+  _shards: {
+    total: 1,
+    successful: 1,
+    skipped: 0,
+    failed: 0
+  },
+  hits: {
+    total: {
+      value: 37,
+      relation: 'eq'
+    },
+    max_score: 12.129974,
+    hits: [
+      {
+        _index: 'test-index2',
+        _id: '1a9844da-0262-49c0-9194-1496f9cfc4ed',
+        _score: 12.129974,
+        _source: {
+          content_id: '1a9844da-0262-49c0-9194-1496f9cfc4ed',
+          section: 'Unit 4: Functions',
+          activity_name: 'Lesson 4.16: Different Types of Sequences',
+          lesson_page: '4.16.3: A Sequence Is a Type of Function',
+          lesson_page_type: 'content',
+          teacher_only: false
+        },
+        highlight: {
+          lesson_page: [
+            '4.16.3: A Sequence <strong>Is</strong> a Type of Function'
+          ],
+          visible_content: [
+            'Activity \n<strong>Jada</strong> and Mai are trying to decide what type of sequence this could be:\n\n\n\n\nterm number\nvalue',
+            '2\n      \n\n\n\n      2\n      \n\n        6\n      \n\n\n\n        5\n      \n\n        18\n      \n\n\n\n<strong>Jada</strong>',
+            'says: \u201cI think this sequence <strong>is</strong> geometric because in the value column, each row <strong>is</strong> 3 times the previous',
+            'Do you agree with <strong>Jada</strong> or Mai? Be prepared to show your reasoning using a graph.',
+            '<strong>Jada</strong> noticed that each value <strong>is</strong> multiplied by 3 to get to the next row, but the table skips terms.'
+          ]
+        }
+      },
+      {
+        _index: 'test-index2',
+        _id: 'f27ad080-9923-48c5-9355-54e4934a95d8',
+        _score: 11.537502,
+        _source: {
+          content_id: 'f27ad080-9923-48c5-9355-54e4934a95d8',
+          section: 'Unit 4: Functions',
+          activity_name: 'Lesson 4.14: Sequences',
+          lesson_page: '4.14.2: What Is a Sequence?',
+          lesson_page_type: 'content',
+          teacher_only: false
+        },
+        highlight: {
+          lesson_page: [
+            '4.14.2: What <strong>Is</strong> a Sequence?'
+          ],
+          visible_content: [
+            'What <strong>is</strong> the smallest number of moves in which you are able to complete the puzzle with 3 discs?',
+            'Enter your answer here:\n\n\nCompare your answer:\n15 moves\n\n\n\n\n\n\n\n\n\n<strong>Jada</strong> says she used the solution for',
+            'Enter your answer here:\n\n\nCompare your answer:\n<strong>Jada</strong> moved the tower of discs 1\u20133, then moved disc 4,',
+            'The number of moves <strong>is</strong> 31 because \\(2\\cdot15+1=31\\).',
+            'The term (of a sequence) <strong>is</strong> one of the numbers in a sequence.'
+          ]
+        }
+      }
+    ]
+  }
+}
+
+jest.mock('../lib/env.ts', () => ({
+  ENV: {
+    OS_RAISE_SEARCHAPI_URL_PREFIX: 'http://searchapi'
+  }
+}))
+// Test passes, but need to clean up the \n and strong tags for testing purposes.
+// Will need to update the url
+describe('search', () => {
+  it('fetches and displays search results from API', async () => {
+    global.fetch = jest.fn().mockResolvedValue({
+      ok: true,
+      json: async () => await Promise.resolve(mockQueryResults)
+    })
+
+    render(
+      <SearchBlock versionId={'12345'} filter={''} />
+    )
+
+    const queryInput = screen.getByRole('textbox')
+    await act(async () => {
+      fireEvent.change(queryInput, { target: { value: 'math' } })
+      fireEvent.click(screen.getByText('Button'))
+    })
+
+    await screen.findAllByText('Unit 4: Functions')
+    expect(await screen.findByText('Lesson 4.16: Different Types of Sequences'))
+    expect(await screen.findByText('Lesson 4.14: Sequences'))
+    expect(global.fetch).toHaveBeenCalledWith(`http://searchapi?q=math&version_id=12345&filter=${''}`)
+  })
+})
