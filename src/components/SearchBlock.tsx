e10b16d793f844c551a40c424e0289b6fd6c8a33
Michael Reyna <MReyna12@live.com>
2024-04-29 22:39:38 -0500

    Add teacher content only filtering

--- src/components/SearchBlock.tsx
+++ src/components/SearchBlock.tsx
@@ -44,6 +44,9 @@ export const SearchBlock = ({ versionId, filter }: SearchBlockProps): JSX.Elemen
   const [query, setQuery] = useState('')
   const [searchResults, setSearchResults] = useState<SearchResults | undefined>(undefined)
   const [errorMessage, setErrorMessage] = useState('')
+  const [teacherContentOnly, setTeacherContentOnly] = useState(false)
+  const [searchTerm, setSearchTerm] = useState('')
+  const [totalHitsDisplayed, setTotalHitsDisplayed] = useState(0)
   const fetchContent = async (): Promise<void> => {
     try {
       const response = filter !== undefined
@@ -68,6 +71,16 @@ export const SearchBlock = ({ versionId, filter }: SearchBlockProps): JSX.Elemen
     }
   }, [])
 
+  const calculateTotalTeacherOnlyHits = (searchResults: SearchResults): void => {
+    let totalTeacherOnlyHits: number = 0
+    searchResults.hits.hits.forEach((hit) => {
+      if (hit._source.teacher_only) {
+        totalTeacherOnlyHits += 1
+      }
+    })
+    setTotalHitsDisplayed(totalTeacherOnlyHits)
+  }
+
   const handleSubmit = async (): Promise<void> => {
     if (query.trim() === '') {
       setErrorMessage('Input cannot be empty')
@@ -75,6 +88,8 @@ export const SearchBlock = ({ versionId, filter }: SearchBlockProps): JSX.Elemen
     }
     setSearchResults(undefined)
     setErrorMessage('')
+    setSearchTerm(query)
+    setTeacherContentOnly(false)
     await fetchContent()
   }
   return (
@@ -112,35 +127,60 @@ export const SearchBlock = ({ versionId, filter }: SearchBlockProps): JSX.Elemen
         )}
       </Formik>
       {searchResults !== undefined && searchResults.hits.total.value !== 0 &&
-        <div className='os-search-results-container'>
-          <p>Total search results: {searchResults.hits.total.value}</p>
-          <p>Total search results displayed: {searchResults.hits.hits.length}</p>
-          <ul className='os-search-results-list'>
-            {searchResults.hits.hits.map((hit) => (
-              <li className='os-search-results-list-item' key={hit._id}>
-                <div>
-                  <h3>Location</h3>
-                  <p>{hit._source.section}</p>
-                  <p>{hit._source.activity_name}</p>
-                  <p>{hit._source.lesson_page !== '' && hit._source.lesson_page}</p>
+        <>
+        <div className='os-search-results-count-container'>
+          <h3>Search Results</h3>
+          <div>
+            <p className='os-search-magnifying-glass'>Displaying {teacherContentOnly ? totalHitsDisplayed : searchResults.hits.hits.length} of out {searchResults.hits.total.value} results for <span className='os-raise-text-bold'>{searchTerm}</span></p>
+            <div className='os-raise-d-flex-nowrap os-raise-justify-content-evenly os-search-teacher-content-toggle-container'>
+              <p className='os-raise-mb-0'>Teacher Content Only</p>
+              <div className='os-raise-bootstrap'>
+                <div className="form-check form-switch">
+                  <input className="form-check-input" type="checkbox" id="flexSwitchCheckDefault" onChange={() => { setTeacherContentOnly(!teacherContentOnly); calculateTotalTeacherOnlyHits(searchResults) }} />
                 </div>
-                <div>
-                  {/* The keys for each item below are generated using the item's index in the array */}
-                  <h3>{hit._source.teacher_only ? 'Teacher Content' : 'Content'}</h3>
-                  {hit.highlight.visible_content?.map((content: string) => (
-                    <p ref={contentRefCallback} className='os-search-results-highlights' dangerouslySetInnerHTML={{ __html: content }}></p>
-                  ))}
-                  {hit.highlight.lesson_page?.map((page: string) => (
-                    <p ref={contentRefCallback} className='os-search-results-highlights' dangerouslySetInnerHTML={{ __html: page }}></p>
-                  ))}
-                  {hit.highlight.activity_name?.map((activity: string) => (
-                    <p ref={contentRefCallback} className='os-search-results-highlights' dangerouslySetInnerHTML={{ __html: activity }}></p>
-                  ))}
-                </div>
-              </li>
-            ))}
-          </ul>
+              </div>
+            </div>
+          </div>
         </div>
+        <div className='os-search-results-container'>
+          {teacherContentOnly
+            ? <ul className='os-search-results-list'>
+                {searchResults.hits.hits.map((hit) => (hit._source.teacher_only &&
+                  <li className='os-search-results-list-item' id={hit._id}>
+                    <div>
+                      <h3>{'Teacher Content'}</h3>
+                      {hit._source.teacher_only && hit.highlight.visible_content?.map((content: string) => (
+                        <p ref={contentRefCallback} className='os-search-results-highlights' dangerouslySetInnerHTML={{ __html: content }}></p>
+                      ))}
+                      {hit._source.teacher_only && hit.highlight.lesson_page?.map((page: string) => (
+                        <p ref={contentRefCallback} className='os-search-results-highlights' dangerouslySetInnerHTML={{ __html: page }}></p>
+                      ))}
+                      {hit._source.teacher_only && hit.highlight.activity_name?.map((activity: string) => (
+                        <p ref={contentRefCallback} className='os-search-results-highlights' dangerouslySetInnerHTML={{ __html: activity }}></p>
+                      ))}
+                    </div>
+                  </li>))}
+              </ul>
+            : <ul className='os-search-results-list'>
+                {searchResults.hits.hits.map((hit) => (
+                  <li className='os-search-results-list-item' id={hit._id}>
+                    <div>
+                      <h3>{hit._source.teacher_only ? 'Teacher Content' : 'Content'}</h3>
+                      {hit.highlight.visible_content?.map((content: string) => (
+                        <p ref={contentRefCallback} className='os-search-results-highlights' dangerouslySetInnerHTML={{ __html: content }}></p>
+                      ))}
+                      {hit.highlight.lesson_page?.map((page: string) => (
+                        <p ref={contentRefCallback} className='os-search-results-highlights' dangerouslySetInnerHTML={{ __html: page }}></p>
+                      ))}
+                      {hit.highlight.activity_name?.map((activity: string) => (
+                        <p ref={contentRefCallback} className='os-search-results-highlights' dangerouslySetInnerHTML={{ __html: activity }}></p>
+                      ))}
+                    </div>
+                  </li>))}
+              </ul>
+          }
+        </div>
+      </>
       }
       {searchResults !== undefined && searchResults.hits.total.value === 0 &&
         <div>
