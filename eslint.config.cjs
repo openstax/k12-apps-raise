c6fc83d70d9e006554154ad775feebe69a1db628
Michael Reyna <MReyna12@live.com>
2024-06-05 10:29:31 -0500

    Update config love to match ROPE version

---
+++ eslint.config.cjs
@@ -0,0 +1,38 @@
+module.exports = [
+  {
+    ...require('eslint-config-love')
+  },
+  {
+    files: [
+      '**/*.ts',
+      '**/*.tsx'
+    ],
+    rules: {
+      '@typescript-eslint/triple-slash-reference': 'off',
+      '@typescript-eslint/space-before-function-paren': [
+        'error',
+        {
+          anonymous: 'always',
+          named: 'never',
+          asyncArrow: 'always'
+        }
+      ],
+      '@typescript-eslint/ban-tslint-comment': 'off',
+      '@typescript-eslint/space-infix-ops': 'error'
+    }
+  },
+  {
+    ignores: [
+      'dist/**',
+      'vite.config.ts',
+      'jest.config.ts',
+      'jest.setup.ts',
+      'jest.polyfills.js',
+      'jest.resolver.js',
+      'playwright.config.ts',
+      'coverage',
+      'eslint.config.cjs'
+    ]
+
+  }
+]
