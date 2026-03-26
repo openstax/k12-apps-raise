e7fbbf8639a2e0ef8893d96ca4ad484ae695a650
Prabhdip Gill <pg53@rice.edu>
2026-03-26 16:56:50 -0700

    Update desmos version

--- src/lib/desmos.ts
+++ src/lib/desmos.ts
@@ -1,6 +1,6 @@
 import { loadScriptTag } from './utils'
 
-export const DESMOS_URL = 'https://www.desmos.com/api/v1.7/calculator.js?apiKey=dcb31709b452b1cf9dc26972add0fda6'
+export const DESMOS_URL = 'https://www.desmos.com/api/v1.11/calculator.js?apiKey=dcb31709b452b1cf9dc26972add0fda6'
 
 export const loadDesmos = async (): Promise<void> => {
   if (window.Desmos === undefined) {
